351c43436488116b1715185540231f740a6dfc30
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-21 19:56:30 +0330

    1bahman

--- past/calc2.py
+++ past/calc2.py
@@ -19,7 +19,7 @@ class Calculator(tk.Frame):
 
     def __init__(self, master):
         master.title("Calculator")
-        master.geometry('245x440')
+        master.geometry('245x500')
         master.resizable(False, False)
 
         self.equation = tk.StringVar()
@@ -70,29 +70,29 @@ class Calculator(tk.Frame):
 
         button_0 = ttk.Button(
             master, text='0', command=lambda: self.show('0'), bootstyle="info, outline", padding=(0, 15))
-        button_0.grid(row=4, column=0, pady=3, padx=(5, 2), sticky="ew")
+        button_0.grid(row=5, column=0, pady=3, padx=(5, 2), sticky="ew")
         button_point = ttk.Button(
             master, text='.', command=lambda: self.show('.'), bootstyle="info, outline", padding=(0, 15))
-        button_point.grid(row=4, column=1, padx=2, pady=3, sticky="ew")
+        button_point.grid(row=5, column=1, padx=2, pady=3, sticky="ew")
         button_add = ttk.Button(
             master, text='+', command=lambda: self.show('+'), bootstyle="info, outline", padding=(0, 15))
-        button_add.grid(row=4, column=2, pady=3, padx=2, sticky="ew")
+        button_add.grid(row=5, column=2, pady=3, padx=2, sticky="ew")
 
         button_sub = ttk.Button(
             master, text='-', command=lambda: self.show('-'), bootstyle="secondary, outline", padding=(0, 15))
-        button_sub.grid(row=5, column=0, pady=3, padx=(5, 2), sticky="ew")
+        button_sub.grid(row=6, column=0, pady=3, padx=(5, 2), sticky="ew")
         button_div = ttk.Button(
             master, text='÷', command=lambda: self.show('/'), bootstyle="secondary, outline", padding=(0, 15))
-        button_div.grid(row=5, column=1, pady=3, padx=2, sticky="ew")
+        button_div.grid(row=6, column=1, pady=3, padx=2, sticky="ew")
         button_mul = ttk.Button(
             master, text='×', command=lambda: self.show('*'), bootstyle="secondary, outline", padding=(0, 15))
-        button_mul.grid(row=5, column=2, pady=3, padx=2, sticky="ew")
+        button_mul.grid(row=6, column=2, pady=3, padx=2, sticky="ew")
         button_clear = ttk.Button(
             master, text='c', command=self.clear, bootstyle="success, outline", padding=(0, 15))
-        button_clear.grid(row=6, column=0, pady=3, padx=(5, 2), sticky="ew")
+        button_clear.grid(row=7, column=0, pady=3, padx=(5, 2), sticky="ew")
         button_eq = ttk.Button(
             master, text='=', command=self.solve, bootstyle="primary", padding=(0, 15))
-        button_eq.grid(row=6, column=1, pady=3, padx=2,
+        button_eq.grid(row=7, column=1, pady=3, padx=2,
                        columnspan=2, sticky="ew")
 
 
