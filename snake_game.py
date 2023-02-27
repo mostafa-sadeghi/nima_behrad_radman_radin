f21ceb4cff82f3b1a64288e2c6baebbc5ca5d381
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-27 19:58:41 +0330

    sadeghi

--- snake_game.py
+++ snake_game.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 from PIL import Image, ImageTk
 MOVES_PER_SPEED = 25
 GAME_SPEED = 1000//MOVES_PER_SPEED
@@ -8,7 +9,7 @@ class Snake(tk.Canvas):
     def __init__(self):
         super().__init__(width=600, height=620, background="black", highlightthickness=0)
         self.snake_positions = [(100, 100), (80, 100), (60, 100)]
-        self.food_position = (200, 100)
+        self.food_position = self.set_new_food_position()
         self.score = 0
         self.direction = "Right"
         self.bind_all("<Key>", self.on_key_press)
@@ -18,7 +19,10 @@ class Snake(tk.Canvas):
 
     def on_key_press(self, e):
         new_direction = e.keysym
-        self.direction = new_direction
+        all_directions = ('Up', 'Down', 'Left', 'Right')
+        opposites = ({"Up", "Down"}, {"Left", "Right"})
+        if new_direction in all_directions and {new_direction, self.direction} not in opposites:
+            self.direction = new_direction
 
     def create_objects(self):
         self.create_text(
@@ -64,6 +68,7 @@ class Snake(tk.Canvas):
     def perform_actions(self):
         if self.check_collisions():
             return
+        self.check_food_collision()
         self.move_snake()
         self.after(GAME_SPEED, self.perform_actions)
 
@@ -73,6 +78,33 @@ class Snake(tk.Canvas):
             head_x_position in (0, 600) or head_y_position in (20, 620) or (
                 head_x_position, head_y_position) in self.snake_positions[1:]
         )
+
+    def check_food_collision(self):
+        if self.snake_positions[0] == self.food_position:
+            self.score = self.score + 1
+            self.snake_positions.append(self.snake_positions[-1])
+            self.create_image(
+                *self.snake_positions[-1], image=self.snake_body, tag="snake")
+
+            self.food_position = self.set_new_food_position()
+            self.coords(self.find_withtag("food"), self.food_position)
+            score = self.find_withtag("score")
+            self.itemconfigure(score, text=f"Score: {self.score}", tag="score")
+
+    def set_new_food_position(self):
+        while True:
+            x_position = random.randint(1, 29) * 20
+            y_position = random.randint(3, 20) * 20
+            food_position = (x_position, y_position)
+            if food_position not in self.snake_positions:
+                return food_position
+
+    def end_game(self):
+        self.delete(tk.ALL)
+        self.create_text(self.winfo_width()/2, self.winfo_height()/2,
+                         text=f"Game Over! Your Score:{self.score}", fill="#fff", font=('TkDefaultFont', 24))
+
+        //
 
 
 root = tk.Tk()
