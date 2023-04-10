a5357cd58d589c4b885e47dd34265738fd448e5b
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-10 19:57:39 +0330

    sadeghi

--- bitmap.py
+++ bitmap.py
@@ -10,4 +10,13 @@ bitmap = """
 """
 print('enter some thing')
 message = input('> ')
-if 
+if message == '':
+    exit()
+
+for line in bitmap.splitlines():
+    for index, pixel in enumerate(line):
+        if pixel == ' ':
+            print(' ', end='')
+        else:
+            print(message[index % len(message)], end='')
+    print()
