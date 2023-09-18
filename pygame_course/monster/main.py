932931be220a5c0533c13cbff4edb34895a37d86
msa <msai.sadeghi@gmail.com>
2023-09-18 19:57:43 +0330

    s

---
+++ pygame_course/monster/main.py
@@ -0,0 +1,17 @@
+import pygame
+
+from config import WINDOW_HEIGHT, WINDOW_WIDTH, FPS
+pygame.init()
+
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+    clock.tick(FPS)
